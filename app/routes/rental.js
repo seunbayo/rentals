3ea9b9465f76794656d8960cb11d0ac58f59df6f
seunbayo <seunbayo83@gmail.com>
2020-12-03 14:24:00 +0100

    finished with the input search buton

--- app/routes/rental.js
+++ app/routes/rental.js
@@ -1,12 +1,11 @@
-import Route from '@ember/routing/route';
+import Route from "@ember/routing/route";
 
-import { inject as service } from '@ember/service';
-
+import { inject as service } from "@ember/service";
 
 export default class RentalRoute extends Route {
   @service store;
 
   async model(params) {
-    return this.store.findRecord('rental', params.rental_id);
+    return this.store.findRecord("rental", params.rental_id);
   }
 }
