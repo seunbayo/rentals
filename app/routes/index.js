23e05136546b59316c4324b884c7dec2c0e0d2d1
seunbayo <seunbayo83@gmail.com>
2020-12-03 12:10:23 +0100

    added a route for each camp

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,26 @@
-import Route from "@ember/routing/route";
+import Route from '@ember/routing/route';
 
-import { inject as service } from "@ember/service";
+const COMMUNITY_CATEGORIES = [
+  'Condo',
+  'Townhouse',
+  'Apartment'
+];
 
 export default class IndexRoute extends Route {
-  @service store;
-
   async model() {
-    return this.store.findAll("rental");
+    let response = await fetch('/api/rentals.json');
+    let { data } = await response.json();
+
+    return data.map(model => {
+      let { id, attributes } = model;
+      let type;
+
+      if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
+        type = 'Community';
+      } else {
+        type = 'Standalone';
+      }
+      return { id, type, ...attributes };
+    });
   }
 }
